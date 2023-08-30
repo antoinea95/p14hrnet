be84481355406bf99d4bec0b64cfb5ae231c0d84
antoinea95 <antoineamr95@gmail.com>
2023-08-30 15:20:09 +0200

    Create text input component

---
+++ src/components/Form/input/Text.jsx
@@ -0,0 +1,29 @@
+import { TextField } from "@mui/material";
+import { Controller } from "react-hook-form";
+import React from 'react';
+
+
+
+export default function Text({name, label, control, errors, ...others}) {
+
+    const error=errors[name]
+    const errorMessage = error && error.message
+    
+
+    return (
+        <Controller
+        name={name}
+        control={control}
+        defaultValue=""
+        render={({ field }) => (
+          <TextField
+            {...field} // Spread the field props to the TextField
+            label={label}
+            error={error ? true : false}
+            helperText={errorMessage}
+            {...others}
+          />
+        )}
+      />
+    )
+}
